5a8bed1be82c756bda5b9b1d020461da146b52c8
Linrty <linrty@126.com>
2022-03-22 21:00:27 +0800

    初始化零售管理后台

---
+++ src/main/java/com/linrty/goods/GoodsManageServerApplication.java
@@ -0,0 +1,13 @@
+package com.linrty.goods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodsManageServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoodsManageServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/linrty/goods/GoodsManageServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linrty.goods;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodsManageServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
